cc691604d8089cabedb778482f7eb609b8d82234
Shivam-9992 <rajputshivam9992@gmail.com>
2025-01-22 15:32:27 +0530

    optimised ths code

--- FrequencyCount.java
+++ FrequencyCount.java
@@ -1,19 +1,22 @@
+import java.util.Arrays;
+
 public class FrequencyCount {
     public static void main(String[] args) {
         int numbers[] = { 1, 2, 3, 2, 1, 5, 1, 2, 3, 3, 3 };
 
+        Arrays.sort(numbers);
+
         System.out.println("Frequency of each integer:");
 
-        for (int i = 0; i < numbers.length; i++) {
-            int count = 0;
-
-            for (int j = 0; j < numbers.length; j++) {
-                if (numbers[i] == numbers[j]) {
-                    count++;
-                }
+        int count = 1;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] == numbers[i - 1]) {
+                count++;
+            } else {
+                System.out.println(numbers[i - 1] + ": " + count);
+                count = 1; 
             }
-
-            System.out.println(numbers[i] + ": " + count);
         }
+        System.out.println(numbers[numbers.length - 1] + ": " + count);
     }
 }
